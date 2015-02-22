b399a57f053c698cc47f3cacf321bc270a0dc841
rlyshw <rlyshw@gmail.com>
2015-02-22 13:26:47 -0500

    Updated the passerrating method

--- Q33/src/Passer.java
+++ Q33/src/Passer.java
@@ -12,19 +12,28 @@ public class Passer {
 		this.COMP = COMP;
 		this.ATT = ATT;
 	}
-	public double a(){
-		return (this.COMP/this.ATT-.3)*5;
+
+	public double a() {
+		return (this.COMP / this.ATT - .3) * 5;
 	}
-	public double b(){
-		return (this.YD/this.ATT-3)*.25;
+
+	public double b() {
+		return (this.YD / this.ATT - 3) * .25;
 	}
-	public double c(){
-		return (this.TD/this.ATT)*20;
+
+	public double c() {
+		return (this.TD / this.ATT) * 20;
 	}
-	public double d(){
-		return 2.375-((this.INT/this.ATT)*25);
+
+	public double d() {
+		return 2.375 - ((this.INT / this.ATT) * 25);
 	}
-	public double passerRating(){
-		return (a()+b()+c()+d())/6;
+	
+	public double mm(double val){
+		return Math.max(0, Math.min(val,2.375));
+	}
+
+	public double passerRating() {
+		return (mm(a()) + mm(b()) + mm(c()) + mm(d())) / 6;
 	}
 }
